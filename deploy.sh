23f87265e204aec40424a516e322b2494600cd2f
Miguel Ledesma <103437837+Xcorpi0@users.noreply.github.com>
2023-02-23 22:57:18 +0100

    modifico gh-pages

--- deploy.sh
+++ deploy.sh
@@ -24,5 +24,5 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:MiguelLedesmaC/pricing-component-with-toogle.git main:gh-pages
+# git push -f git@github.com:MiguelLedesmaC/Pricing-component-React.git main:gh-pages
 
